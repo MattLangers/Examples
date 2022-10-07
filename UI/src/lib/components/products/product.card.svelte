cd3c0e9f8bb9922b7e4524bf39afaa66323928d8
ML <mattlangers@gmail.com>
2022-10-07 12:00:01 +0100

    Improve product card look/feel
    Create new product type component

--- UI/src/lib/components/products/product.card.svelte
+++ UI/src/lib/components/products/product.card.svelte
@@ -1,45 +1,98 @@
 <script lang="ts">
-	import type { Product } from "$lib/models/product";
+	import type { Product } from '$lib/models/product';
+    import ProductTypeIconComponent from '$lib/components/products/product.type.icon.svelte'
 
 	export let product: Product;
 </script>
 
 <div class="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md">
-    <div class="flex items-center justify-between">
-        <h1 class="mt-2 text-lg font-semibold text-gray-800">{product.name}</h1>
-        <span class="px-3 py-1 text-xs text-blue-800 uppercase bg-red-200 rounded-full">{product.productType.name}</span>
-    </div>
+	<div class="flex items-center justify-between">
+		<h1 class="mt-2 text-lg font-semibold text-gray-800">{product.name}</h1>
+		<ProductTypeIconComponent productType={product.productType}></ProductTypeIconComponent>
+	</div>
 
-    <div>
-        <p class="mt-2 text-sm text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eligendi similique exercitationem optio libero vitae accusamus cupiditate laborum eos.</p>
-    </div>
+	<div>
+		<p class="mt-2 text-sm text-gray-600">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio eligendi similique
+			exercitationem optio libero vitae accusamus cupiditate laborum eos.
+		</p>
+	</div>
 
-    <div>
-        <div class="flex mt-2 item-center">
-            <svg class="w-5 h-5 text-gray-700 fill-current" viewBox="0 0 24 24">
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"></path>
-            </svg>
+	<div>
+		<div class="flex mt-2 item-center">
+			<svg class="w-5 h-5 text-gray-700 fill-current" viewBox="0 0 24 24">
+				<path
+					d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+				/>
+			</svg>
 
-            <svg class="w-5 h-5 text-gray-700 fill-current" viewBox="0 0 24 24">
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"></path>
-            </svg>
+			<svg class="w-5 h-5 text-gray-700 fill-current" viewBox="0 0 24 24">
+				<path
+					d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+				/>
+			</svg>
 
-            <svg class="w-5 h-5 text-gray-700 fill-current" viewBox="0 0 24 24">
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"></path>
-            </svg>
+			<svg class="w-5 h-5 text-gray-700 fill-current" viewBox="0 0 24 24">
+				<path
+					d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+				/>
+			</svg>
 
-            <svg class="w-5 h-5 text-gray-300 fill-current" viewBox="0 0 24 24">
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"></path>
-            </svg>
+			<svg class="w-5 h-5 text-gray-300 fill-current" viewBox="0 0 24 24">
+				<path
+					d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+				/>
+			</svg>
 
-            <svg class="w-5 h-5 text-gray-300 fill-current" viewBox="0 0 24 24">
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"></path>
-            </svg>
-        </div>
-    </div>
+			<svg class="w-5 h-5 text-gray-300 fill-current" viewBox="0 0 24 24">
+				<path
+					d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
+				/>
+			</svg>
+		</div>
+	</div>
 
-    <div class="flex justify-between mt-3 item-center">
-        <h1 class="text-lg font-bold text-gray-700">£1.00</h1>
-        <button class="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm">Edit</button>
-    </div>
+	<div class="flex justify-between mt-3 item-center">
+		<div class="flex flex-row w-full">
+			<div class="basis-3/5">
+				<h1 class="text-lg font-bold text-gray-700">£1.00</h1>
+			</div>
+			<div class="basis-1/5 icon_alignment">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+					/>
+				</svg>
+			</div>
+			<div class="basis-1/5 icon_alignment">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+				</svg>
+			</div>
+		</div>
+	</div>
 </div>
+
+<style>
+	.icon_alignment {
+		display: flex;
+		align-items: right;
+		justify-content: right;
+	}
+</style>
---
+++ UI/src/lib/components/products/product.type.icon.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import type { ProductType } from '$lib/models/productType';
+
+	export let productType: ProductType;
+
+	function getClass(id: number) {
+		switch (id) {
+			case 1: {
+				return 'bg-red-600';
+			}
+
+			case 2: {
+				return 'bg-orange-400';
+			}
+
+			case 3: {
+				return 'bg-amber-400';
+			}
+
+			case 4: {
+				return 'bg-yellow-300';
+			}
+
+			case 5: {
+				return 'bg-lime-400';
+			}
+
+			case 6: {
+				return 'bg-green-400';
+			}
+
+			case 7: {
+				return 'bg-sky-400';
+			}
+
+			case 8: {
+				return 'bg-blue-600';
+			}
+
+			case 9: {
+				return 'bg-purple-400';
+			}
+
+			case 10: {
+				return 'bg-pink-400';
+			}
+
+			case 11: {
+				return 'bg-rose-900';
+			}
+
+			case 12: {
+				return 'bg-slate-400';
+			}
+
+			default: {
+				return 'bg-red-200';
+			}
+		}
+	}
+</script>
+
+<span class="px-3 py-1 text-xs text-white uppercase rounded-full {getClass(productType.id)}"
+	>{productType.name}</span
+>
