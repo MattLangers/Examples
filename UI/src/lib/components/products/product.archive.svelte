9fe6f1ef7753159fe8860c71a6ba1616d87fb2db
ML <mattlangers@gmail.com>
2022-10-10 20:23:09 +0100

    Adding a edit and archive animation
    Disabling the submit button and showing a processing SVG in the button

--- UI/src/lib/components/products/product.archive.svelte
+++ UI/src/lib/components/products/product.archive.svelte
@@ -6,16 +6,15 @@
 	export let product: Product;
 	import { products as productsStore } from '$lib/components/products/product.store';
 
-	let products: Product[] = [];
+	let archiveButtonDisabled = false;
 
-	productsStore.subscribe((p) => {
-		products = p;
-	});
+	productsStore.subscribe(() => {});
 
 	const dispatch = createEventDispatcher();
 
 	async function handleArchive() {
 		if (product.id.length > 0) {
+			archiveButtonDisabled = true;
 			var result = await archiveProduct(product.id);
 			if (result) {
 				$productsStore = $productsStore.filter(function (value, index, arr) {
@@ -23,6 +22,7 @@
 				});
 				dispatch('close');
 			}
+			archiveButtonDisabled = false;
 		}
 	}
 
@@ -86,7 +86,28 @@
 						<button
 							type="button"
 							class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-							on:click={handleArchive}>Archive</button
+							on:click={handleArchive}
+							disabled={archiveButtonDisabled}>
+							<svg
+								class="animate-spin -ml-1 mr-3 h-5 w-5 text-white { archiveButtonDisabled ? '' : 'processing_edit_request'}"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<circle
+									class="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									stroke-width="4"
+								/>
+								<path
+									class="opacity-75"
+									fill="currentColor"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+								/>
+							</svg>Archive</button
 						>
 						<button
 							type="button"
@@ -99,3 +120,10 @@
 		</div>
 	</div>
 {/if}
+
+<style>
+	.processing_edit_request {
+		visibility: hidden;
+		display: none;
+	}
+</style>
--- UI/src/lib/components/products/product.edit.svelte
+++ UI/src/lib/components/products/product.edit.svelte
@@ -5,41 +5,46 @@
 	import { products as productsStore } from '$lib/components/products/product.store';
 	import { ProductEditObjectBuilder } from './product.edit.object.builder';
 	import type { ProductEdit } from '$lib/models/product.edit';
-	
-	let products: Product[] = [];
-    import { get_store_value } from 'svelte/internal';
-	
+
 	export let open = false;
 	export let product: Product;
-	
-	let productName = product.name
-	let productDescription = product.description
-	let productPrice = product.price
-	let productRanking = product.ranking
+
+	let productName = product.name;
+	let productDescription = product.description;
+	let productPrice = product.price;
+	let productRanking = product.ranking;
 	let productEdit: ProductEdit;
-	
-	productsStore.subscribe((p) => {
-		products = p;
-	});
+	let submitButtonDisabled = false;
+
+	productsStore.subscribe(() => {});
 
 	const dispatch = createEventDispatcher();
 
 	async function handleEdit() {
 		if (product.id.length > 0) {
+			submitButtonDisabled = true;
 			var result = await editProduct(product);
 			if (result) {
 				product.name = productName;
 				product.description = productDescription;
 				product.price = productPrice;
 				product.ranking = productRanking;
-				productsStore.update(contents => [...contents, product]);
+				productsStore.update((contents) => [...contents, product]);
 				dispatch('close');
+				submitButtonDisabled = false;
 			}
 		}
 	}
 
 	var editProduct = async function (product: Product): Promise<boolean> {
-		productEdit = new ProductEditObjectBuilder().create(product, { id: product.id, productType: product.productType ,name: productName, description: productDescription, ranking: productRanking, price: productPrice });
+		productEdit = new ProductEditObjectBuilder().create(product, {
+			id: product.id,
+			productType: product.productType,
+			name: productName,
+			description: productDescription,
+			ranking: productRanking,
+			price: productPrice
+		});
 		const response = await fetch(variables.api_URL + 'products/' + product.id, {
 			method: 'PUT',
 			headers: {
@@ -125,8 +130,8 @@
 										id="edit_product_description"
 										placeholder="Description"
 										rows="4"
-										bind:value={productDescription}></textarea
-									>
+										bind:value={productDescription}
+									/>
 
 									<input
 										type="range"
@@ -178,8 +183,30 @@
 							type="button"
 							class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
 							id="product_edit_submit_button"
-							on:click={handleEdit}>Submit</button
-						>
+							on:click={handleEdit}
+							disabled={submitButtonDisabled}>
+							<svg
+								class="animate-spin -ml-1 mr-3 h-5 w-5 text-white { submitButtonDisabled ? '' : 'processing_edit_request'}"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<circle
+									class="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									stroke-width="4"
+								/>
+								<path
+									class="opacity-75"
+									fill="currentColor"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+								/>
+							</svg>
+							Submit
+						</button>
 						<button
 							type="button"
 							class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
@@ -208,5 +235,10 @@
 	input[type='range'] {
 		width: 200px;
 		margin: 0;
+	}
+
+	.processing_edit_request {
+		visibility: hidden;
+		display: none;
 	}
 </style>
